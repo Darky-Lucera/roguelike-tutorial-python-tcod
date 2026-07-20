d1af789fec2a511da3c9acb765964147f05bde71
Carlos Aragonés <c.aragones@gmail.com>
2026-07-20 21:26:53 +0200

    First step to move from GA to goatcounter

---
+++ v3/javascripts/goatcounter-config.js
@@ -0,0 +1,3 @@
+// Must load before count.js: sets the endpoint it reads instead of the
+// data-goatcounter attribute, which MkDocs' extra_javascript can't add.
+window.goatcounter = { endpoint: 'https://caragones.goatcounter.com/count' };
